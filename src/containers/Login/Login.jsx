bc1e9dbad34b7bdfcf13ad8ffdf9c742ae71dff6
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-28 20:51:43 +0200

    Login functionality implemented succesfully.

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,12 +1,34 @@
-import React from "react"
-import { Container, Form, Button, Card} from "react-bootstrap"
+import React, { useRef, useState } from "react"
+import { Container, Form, Button, Card, Alert} from "react-bootstrap"
+import { useAuth } from '../../contexts/AuthContext';
+import { useHistory } from "react-router-dom"
 import FacebookIcon from '@material-ui/icons/Facebook';
 import EmailIcon from '@material-ui/icons/Email';
 import TwitterIcon from '@material-ui/icons/Twitter';
 
 
 export default function Login() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const { login } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
 
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setError("")
+      setLoading(true)
+      await login(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
+    } catch {
+      setError("Failed to log in")
+    }
+
+    setLoading(false)
+  }
 
 
   return (
@@ -18,18 +40,15 @@ export default function Login() {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign In</h2>
-          <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" required />
+              <Form.Control type="email" ref={emailRef} required />
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" required />
+              <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
             <Button className="w-100" type="submit">
               Sign In
