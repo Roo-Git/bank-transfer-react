502a17ff3d686e1b9493cf585b258ff8dd32ec85
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-05-29 12:54:33 +0200

    Added logout functionality

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Button, Alert } from "react-bootstrap"
+import { useHistory } from 'react-router-dom'
+import { useAuth } from '../../contexts/AuthContext'
 
-function Home() {
+export default function Home() {
+  const [error, setError] = useState("")
+  const { logout } = useAuth()
+  const history = useHistory()
+
+  async function handleLogout() {
+    setError("")
+
+    try {
+      await logout()
+      history.push('/register')
+
+    } catch {
+      setError("Failed to log out")
+    }
+  };
+  
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <>
+    <Card>
+      <Card.Body>
+        <h2 className="text-center mb-4">Bank Transfer</h2>
+        {error && <Alert variant="danger">{error}</Alert>}
+      </Card.Body>
+    </Card>
+    <div className="w-100 text-center mt-2">
+      <Button variant="link" onClick={handleLogout}>
+        LogOut
+      </Button>
     </div>
+  </>
   )
 }
 
-export default Home
+
